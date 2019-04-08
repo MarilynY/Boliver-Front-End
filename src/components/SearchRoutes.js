ca466ac5020ea604227e69aff8348617f2c9d7ca
kamikuroro <p.xywang@gmail.com>
2019-04-07 20:39:45 -0400

    {SHIP} functionalities complete, almost there

--- src/components/SearchRoutes.js
+++ src/components/SearchRoutes.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Popconfirm, Form, Input, Button, message, Skeleton, Card, Modal, Avatar, Spin} from 'antd';
+import {Popconfirm, Form, Input, Button, message, Skeleton, Card, Avatar, Spin} from 'antd';
 import { API_ROOT, TOKEN_KEY, AUTH_HEADER} from '../constants';
-import { Footer } from './Footer.js';
+import { Footer } from './Footer';
+import { CardBot } from './CardBot';
 
 const { Meta } = Card;
 function confirm() {
@@ -10,6 +11,9 @@ function confirm() {
 function ohno() {
   message.info("is that right.");
 }
+/** useful little tools
+   <Spin spinning={this.state.spin} delay={500}></Spin>
+*/
 
 class SearchForm extends React.Component {
     constructor() {
@@ -68,83 +72,16 @@ class SearchForm extends React.Component {
         });
     }
 
-    showModal = () => {
-      this.setState({
-        visible: true,
-      });
-    }
-
-    handleOk = () => {
-      const{ searchRes } = this.state;
-      const test = {
-        origin : searchRes.DeliveryAddress.origin,
-        destination : searchRes.DeliveryAddress.destination,
-        travel_time : String(searchRes.GroundBot.travel_time),
-        cost : String(searchRes.GroundBot.cost),
-        sender : "pipiMei",
-        receiver : "pipiKo",
-        address : searchRes.GroundBot.base,
-        type : "ground",
-      }
-      console.log("before firing submitOrder API: " + JSON.stringify(test));
-
-      // //Fire API Call
-      // const token = localStorage.getItem(TOKEN_KEY);
-      // fetch(`${API_ROOT}/submitorder`, {
-      //   method: 'POST',
-      //   headers:{
-      //     Authorization: `${AUTH_HEADER} ${token}`
-      //   },
-      //   body: JSON.stringify({
-      //     origin : test.origin,
-      //     destination : test.destination,
-      //     travel_time : test.travel_time,
-      //     cost : test.cost,
-      //     sender : test.sender,
-      //     receiver : test.receiver,
-      //     address : test.address,
-      //     type : test.type
-      //     ,
-      //   }),
-      // })
-      //   .then((response) => {
-      //       if (response.ok) {
-      //           return response.json();
-      //       }
-      //       throw new Error(response.statusText)
-      //   })
-      //   .then ((data) => {
-      //       this.setState({
-      //         loading: false,
-      //         spin: false,
-      //         visible: false,
-      //       })
-      //       message.success("your order has been successfully placed!")
-      //       console.log(data)
-      //   })
-      //   .catch((e) => {
-      //       message.error("something went wrong when trying to place your order _(¦3」∠)_")
-      //       console.log(e)
-      //   })
-    }
-
-    handleCancel = () => {
-      console.log('Clicked cancel button');
-      this.setState({
-        visible: false,
-      });
-    }
-
     setSpin = () => {
       this.setState({ spin: true });
     }
- 
-    /** useful little tools
-       <Spin spinning={this.state.spin} delay={500}></Spin>
-    */
+
+    offSpin = () => {
+      this.setState({ spin: false });
+    }
 
     deliveryOptions = () => {
-      const { searchRes, loading, confirmLoading, visible} = this.state;
+      const { searchRes, loading } = this.state;
 
       if(searchRes == null){
         return(
@@ -152,26 +89,26 @@ class SearchForm extends React.Component {
           <div className="divTableCell">
             <div className="cardLeft">
               <Card className="CardLoading" actions={[
-                    <Popconfirm 
-                      title={"So you like to roll?"} 
-                      onConfirm={confirm} 
-                      onCancel={ohno} 
-                      okText="Yes"
-                      cancelText="No"
-                    >
-                      <Button >Choose GroundBot</Button>
-                    </Popconfirm>]}
-                  > 
-                    <Spin spinning={this.state.spin} delay={500}>
-                    <Skeleton loading={loading} avatar active>
-                      <Meta
-                        avatar={<Avatar  src={require("../assets/images/groundBot.png" )} />}
-                        title="Card title"
-                        description=""
-                      />
-                    </Skeleton>
-                    </Spin>
-                  </Card>
+                <Popconfirm 
+                  title={"So you like to roll?"} 
+                  onConfirm={confirm} 
+                  onCancel={ohno} 
+                  okText="Yes"
+                  cancelText="No"
+                >
+                  <Button >Choose GroundBot</Button>
+                </Popconfirm>]}
+              > 
+                <Spin spinning={this.state.spin} delay={200}>
+                <Skeleton loading={loading} avatar active>
+                  <Meta
+                    avatar={<Avatar  src={require("../assets/images/groundBot.png" )} />}
+                    title="Card title"
+                    description=""
+                  />
+                </Skeleton>
+                </Spin>
+              </Card>
             </div>
           </div>
           <div className="divTableCell">
@@ -187,7 +124,7 @@ class SearchForm extends React.Component {
                   <Button >Choose Drone</Button>
                 </Popconfirm>]}
               >
-                <Spin spinning={this.state.spin} delay={500}>
+                <Spin spinning={this.state.spin} delay={200}>
                 <Skeleton loading={loading} avatar active>
                   <Meta
                     avatar={<Avatar src={require("../assets/images/drone.jpg")} />}
@@ -201,85 +138,34 @@ class SearchForm extends React.Component {
           </div>
           </div></div></div>
         )
-      }else if(searchRes != null){
+      }else {
         return(
           <div className="divTable"><div className="divTableBody"><div className="divTableRow">
             <div className="divTableCell">
               <div className="cardLeft">
-                <Card
-                  className="Card"
-                  actions={[<Button loading={loading} onClick={this.showModal}>Choose GroundBot</Button>]}
-                > 
-                  <Spin spinning={this.state.spin} delay={500}>
-                    <Modal title="Title: Ground Bot"
-                           centered
-                           visible={visible}
-                           onOk={this.handleOk}                     
-                           okText="Confirm"
-                           confirmLoading={confirmLoading}
-                           onCancel={this.handleCancel}
-                    >
-                      <p>{searchRes.DeliveryAddress.origin}</p>
-                      <p>{searchRes.DeliveryAddress.destination}</p>
-                      <p>{searchRes.GroundBot.pickup_time}</p>
-                    </Modal>
-                  </Spin>
-                  <Skeleton loading={loading} avatar active>
-                    <Meta
-                      avatar={<Avatar  src={require("../assets/images/groundBot.png" )} />}
-                      // title="Ground Robot"
-                      description= {
-                        <div>
-                          <p><b>origin: </b>{searchRes.DeliveryAddress.origin}</p>
-                          <p><b>destination: </b>{searchRes.DeliveryAddress.destination}</p>
-                          <p><b>cost: </b>{searchRes.GroundBot.cost} dollar</p>
-                          <p><b>pickup time: </b>{searchRes.GroundBot.pickup_time} minutes</p>
-                          <p><b>travel time: </b> {searchRes.GroundBot.travel_time} minutes</p>
-                          <p><b>travel distance: </b> {searchRes.GroundBot.travel_distance} miles</p>
-                          <p><b>dispatch center: </b>{searchRes.GroundBot.base}</p>       
-                        </div>
-                      }
-                    />
-                  </Skeleton>
-                </Card>
+              <Spin spinning={this.state.spin} delay={200}>
+                <CardBot 
+                  botResult = {searchRes.GroundBot} 
+                  robotType = {"ground"} 
+                  address = {searchRes.DeliveryAddress} 
+                  cLoading = {loading}
+                  botAvatar = {<Avatar src={require("../assets/images/groundBot.png")} />}
+                  chooseButton = {"Choose GroundBot"} 
+                />
+              </Spin>
               </div>      
             </div>
             <div className="divTableCell">
               <div className="cardRight">
-                <Card
-                  className="Card"
-                  actions={[<Button loading={loading} onClick={this.showModal}>Choose Drone</Button>]}
-                >
-                  <Modal title="Title: Drone"
-                          centered
-                          visible={visible}
-                          onOk={this.handleOk}
-                          okText="Confirm"
-                          confirmLoading={confirmLoading}
-                          onCancel={this.handleCancel}
-                  >
-                    <p>{searchRes.DeliveryAddress.origin}</p>
-                    <p>{searchRes.DeliveryAddress.destination}</p>
-                    <p>{searchRes.Drone.pickup_time}</p>
-                  </Modal>
-                  <Skeleton loading={loading} avatar active>
-                    <Meta
-                      avatar={<Avatar src={require("../assets/images/drone.jpg")} />}
-                      // title="Drone"
-                      description={
-                        <div>
-                          <p><b>origin: </b>{searchRes.DeliveryAddress.origin}</p>
-                          <p><b>destination: </b>{searchRes.DeliveryAddress.destination}</p>
-                          <p><b>cost: </b>{searchRes.Drone.cost} dollar</p>
-                          <p><b>pickup time: </b>{searchRes.Drone.pickup_time} minutes</p>
-                          <p><b>travel time: </b> {searchRes.Drone.travel_time} minutes</p>
-                          <p><b>travel distance: </b> {searchRes.Drone.travel_distance} miles</p>
-                          <p><b>dispatch center: </b>{searchRes.Drone.base}</p>       
-                        </div>
-                      }
-                    />
-                  </Skeleton>
-                </Card>
+              <Spin spinning={this.state.spin} delay={200}>
+                <CardBot 
+                  botResult = {searchRes.Drone} 
+                  robotType = {"drone"} 
+                  address = {searchRes.DeliveryAddress}
+                  botAvatar = {<Avatar src={require("../assets/images/drone.jpg")} />}
+                  chooseButton = {"Choose Drone"}
+                />
+              </Spin>
               </div>
             </div>
           </div></div></div>
@@ -295,7 +181,7 @@ class SearchForm extends React.Component {
         <div className="PlaceOrderUI">
           <div className="SearchFormWrapper">
             <div className="SearchForm">
-              <Form layout={"horizontal"} onSubmit={this.handleSubmit} className="search-form">
+              <Form layout={"horizontal"} onSubmit={this.handleSubmit} onClick={this.offSpin} className="search-form">
                 <Form.Item label="Pick-up Addr" {...formItemLayout} >
                   {getFieldDecorator('origin', {
                     rules: [{ required: true, message: 'pick-up address cannot be empty' }],
---
+++ src/components/SenderReceiverForm.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Form, Icon, Input, Tooltip } from 'antd';
+
+const FormItem = Form.Item;
+
+class CreateSenderReceiverForm extends React.Component {
+
+ render() {
+   const { getFieldDecorator } = this.props.form;
+   const formItemLayout = {
+     labelCol: { span: 6 },
+     wrapperCol: { span: 14 },
+   };
+
+   return (
+     <Form>
+       <FormItem {...formItemLayout} label="sender">
+         {getFieldDecorator('sender', {
+           rules: [{
+             required: true,
+             message: 'Sender cannot be empty',
+           }],
+         })(
+            <Input
+                placeholder="Enter name of sender"
+                prefix={<Icon type="smile" theme ="twoTone" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                suffix={
+                <Tooltip title="Sender does not have to be a person">
+                    <Icon type="info-circle" style={{ color: 'rgba(0,0,0,.45)' }} />
+                </Tooltip>
+                }
+            />
+         )}
+       </FormItem>
+       <FormItem {...formItemLayout} label="receiver">
+         {getFieldDecorator('receiver', {
+           rules: [{
+             required: true,
+             message: 'Receiver cannot be empty',
+           }],
+         })(
+            <Input
+                placeholder="Enter name of receiver"
+                prefix={<Icon type="smile" theme ="twoTone" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                suffix={
+                <Tooltip title="Sender does not have to be a person">
+                    <Icon type="info-circle" style={{ color: 'rgba(0,0,0,.45)' }} />
+                </Tooltip>
+                }
+            />
+         )}
+       </FormItem>
+     </Form>
+   );
+ }
+}
+
+export const SenRecForm = Form.create()(CreateSenderReceiverForm);
