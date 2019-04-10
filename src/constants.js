74e342ca20bb7532fc1d7bcf25c32d06bce72196
QingzhouLi <39134900+QingzhouLi@users.noreply.github.com>
2019-04-10 13:34:27 -0400

    deployed backend to EC2 and changed the URL

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const API_ROOT = 'http://localhost:8080/Boliver';
+export const API_ROOT = 'http://3.19.14.171:8080/Boliver';
 export const TOKEN_KEY = 'TOKEN_KEY';
 export const AUTH_HEADER = 'Bearer';
